ab6b2c478bd261422ac79baa13e273af6d1f4535
Mario <mariovillaescusa@hotmail.com>
2019-11-13 13:50:51 +0100

    Esqueleto y primeras vistas

---
+++ resources/views/productos/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Editar el producto {{ $id }}
+
+@stop
---
+++ resources/views/productos/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Muestra el producto {{ $id }}
+
+@stop
