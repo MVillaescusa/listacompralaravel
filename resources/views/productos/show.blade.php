e05e781611d9d0ce25c5f692771c898917ebcd7f
Mario <mariovillaescusa@hotmail.com>
2019-11-20 13:32:44 +0100

    Ejercicios 2

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-    Muestra el producto {{ $id }}
+<div class="row">
+
+    <div class="col-sm-4">
+
+        <img src="https://picsum.photos/200/300/" style="height:200px"/>
+
+    </div>
+    <div class="col-sm-8">
+
+        <h2>{{ $producto[0] }}</h2>
+        <h5>Categoria: {{ $producto[1] }}</h5>
+
+            <p>Estado: Producto actualmente comprado</p>
+            <button type="button" class="btn btn-danger">Pendiente de compra</button>
+
+
+        <button type="button" class="btn btn-warning">Editar producto</button>
+        <button type="button" class="btn btn-default">Volver al listado
+
+    </div>
+</div>
 
 @stop
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'HomeController@getHome');
 
 Route::get('/login', function () {
     return view('login');
@@ -23,17 +21,12 @@ Route::get('/logout', function () {
     return view('logout');
 });
 
-Route::get('/productos', function () {
-    return view('productos.index');
-});
+Route::get('/productos', 'ProductoController@getIndex');
 
-Route::get('/productos/show/{id}', function ($id) {
-    return view('productos.show', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('/productos/show/{id}', 'ProductoController@getShow')
+->where('id', '[0-9]+');
 
-Route::get('/productos/create', function () {
-    return view('productos.create');
-});
-Route::get('/productos/edit/{id}', function ($id) {
-    return view('productos.edit', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('/productos/create', 'ProductoController@getCreate');
+
+Route::get('/productos/edit/{id}', 'ProductoController@getEdit')
+->where('id', '[0-9]+');
